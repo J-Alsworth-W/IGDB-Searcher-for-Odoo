02b46b3810004df2269b7557475a45fe1300bb57
Jacob <61456816+J-Alsworth-W@users.noreply.github.com>
2026-07-24 12:36:47 +0100

    Reworking config to reduce unnecessary access token requests.

--- igdb_search/models/igdb_config.py
+++ igdb_search/models/igdb_config.py
@@ -1,3 +1,6 @@
+import datetime
+from datetime import timedelta
+
 from odoo import api, exceptions, fields, models, modules, _
 import requests
 import logging
@@ -13,22 +16,29 @@ class IgdbConfig(models.Model):
     client_secret = fields.Char("Client Secret", required=True, store=True)
     auth_token_url = fields.Char("Twitch Auth URL", required=True, store=True)
     access_token = fields.Char("Access Token")
+    expiry_time = fields.Datetime("Access Token Expiry Time")
     active = fields.Boolean(default=True)
 
     def test_connection(self):
-        access_token = requests.post(self.auth_token_url, data={'client_id': self.client_id_string,
-                                                                'client_secret': self.client_secret,
-                                                                'grant_type': "client_credentials"})
-
-        if access_token:
-            try:
-                self.access_token = access_token.json().get('access_token')
-            except Exception as e:
-                raise Exception(access_token)
+        if self.expiry_time and self.expiry_time - datetime.datetime.now() >= timedelta(days=1):
+            return self
         else:
-            raise exceptions.ValidationError("Connection failed!")
+            access_token = requests.post(self.auth_token_url, data={'client_id': self.client_id_string,
+                                                                    'client_secret': self.client_secret,
+                                                                    'grant_type': "client_credentials"})
 
-        return self
+            if access_token:
+                try:
+                    response_json = access_token.json()
+                    self.access_token = response_json.get('access_token')
+                    expires_in = response_json.get('expires_in')
+                    self.expiry_time = datetime.datetime.now() + datetime.timedelta(seconds=expires_in)
+                except Exception as e:
+                    raise Exception(access_token)
+            else:
+                raise exceptions.ValidationError("Connection failed!")
+
+            return self
 
     def get_config(self):
         configs = self.env['igdb.config'].search([])
